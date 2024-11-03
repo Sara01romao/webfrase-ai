2641139900805d3a405113d243a7ec6169d48bd2
Sara Romao <sara-romao@live.com>
2024-11-02 22:44:35 -0300

    feat: filtro inicio

--- frontend/webfrases/src/App.tsx
+++ frontend/webfrases/src/App.tsx
@@ -10,61 +10,82 @@ interface GeneratedText {
 function App() {
 
   const [phrase, setPhrase] = useState<GeneratedText[]>([])
+  const [nameDate, setNameDate] =useState('Natal');
 
     useEffect(() => {
-
-      async function loadtxt() {
-        try {
-          const data = {
-            inputs: "Crie uma frase entre curta para a rede social comemorando Dia das Crianças",
-            parameters: { max_new_tokens: 50 },
-          };
-
-          const response = await fetch("http://localhost:3000/query", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(data),
-          });
-
-         
-
-          const result = await response.json();
-          console.log(result); // Exibe o resultado da API no console
-
-
-          setPhrase(result)
-        } catch (error) {
-          console.error("Erro", error); // Loga erros de requisição
-        }
-      
-        console.table(phrase[0].generated_text
-        )
-      }
-
-      loadtxt();
-
-      
+      // loadtxt();
     }, []);
+
+
+    async function loadtxt() {
+      try {
+        const data = {
+          inputs: `Crie uma frase entre curta para a rede social comemorando ${nameDate}`,
+          parameters: { max_new_tokens: 50 },
+        };
+
+        const response = await fetch("http://localhost:3000/query", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        });
+
+      
+        const result = await response.json();
+        console.log(result); 
+
+
+        setPhrase(result)
+      } catch (error) {
+        console.error("Erro", error); 
+      }
+    
+      console.table(phrase[0].generated_text
+      )
+    }
 
   //limpa resultado
   const cleanText = (text: string) => {
      
     const parts = text.split('#');
     return parts[1] ? parts[1].trim() : text;
-
-   
-    
   };
+
+  function handleCreate(){
+    loadtxt()
+  }
+
+  function handleClean(){
+    setPhrase([])
+  }
+
+  function handleFilter(dataNome: string){
+    //  alert(dataNome)
+
+     setNameDate(dataNome);
+     loadtxt()
+    
+  }
+  
   
   return (
   
     <>
       <div>
-      {phrase.map((item, index) => (
-        <p key={index}>{cleanText(item.generated_text)}</p> 
-      ))}
+        {phrase.map((item, index) => (
+          <p key={index}>{cleanText(item.generated_text)}</p> 
+        ))}
+
+        <button onClick={handleCreate}>Gerar Frase</button>
+        <button onClick={handleClean}>Limpar</button>
+
+        <ul className=''>
+          <li onClick={()=>handleFilter('Dia das Crianças')}>Dia das Crianças</li>
+          <li onClick={()=>handleFilter('Halloween')}>Halloween</li>
+          <li onClick={()=>handleFilter('Natal')} >Natal</li>
+        </ul>
       </div>
      
     </>
