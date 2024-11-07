83ff52a0fba468ceefc94bb649d5945a76e1747f
Sara Romao <sara-romao@live.com>
2024-11-06 21:21:49 -0300

    remove codigo comentado

--- backend/index.js
+++ backend/index.js
@@ -47,31 +47,3 @@ app.listen(PORT, () => {
 });
 
 
-// async function query(data) {
-// 	const response = await fetch(
-// 		"https://api-inference.huggingface.co/models/mistralai/Mistral-Nemo-Instruct-2407",
-// 		{
-// 			headers: {
-// 				Authorization: `Bearer  ${process.env.TOKEN}`,
-// 				"Content-Type": "application/json",
-// 			},
-// 			method: "POST",
-// 			body: JSON.stringify(data),
-// 		}
-// 	);
-// 	const result = await response.json();
-// 	return result;
-// }
-
-	// query(
-	// 	{"inputs": "Crie uma frase curta para a rede social comemorando a black friday",
-	// 	 "parameters": { "max_new_tokens": 50, },
-		 
-	// 	 })
-	// 	 .then((response) => {
-	// 		res.send('Hello, World!', JSON.stringify(response, null, 2));
-	// 		console.log(JSON.stringify(response, null, 2));
-	// 	 }).catch((err)=>{
-	// 		console.log(err)
-	// 	 }
-	// );
